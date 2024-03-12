f2d9e9d59c6876f2c5b74936d8b283e5c02c8c9c
Roman Scherzer <r.scherzer@business-code.de>
2024-03-12 15:39:28 +0100

    migrated javax.xml.bind, javax.xml.soap, jaxb

--- Server/src/main/java/de/businesscode/bcdui/binding/subjectFilter/SubjectFilters.java
+++ Server/src/main/java/de/businesscode/bcdui/binding/subjectFilter/SubjectFilters.java
@@ -21,10 +21,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
--- Server/src/main/java/de/businesscode/bcdui/subjectsettings/SubjectSettings.java
+++ Server/src/main/java/de/businesscode/bcdui/subjectsettings/SubjectSettings.java
@@ -25,12 +25,12 @@ import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.logging.log4j.Logger;
