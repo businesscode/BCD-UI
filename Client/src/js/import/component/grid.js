36753dcbc21bad0ced73a9fff9e20f1dc2119243
Roman Scherzer <r.scherzer@business-code.de>
2024-06-04 15:34:23 +0200

    add missing grid dependency

--- Client/src/js/import/component/grid.js
+++ Client/src/js/import/component/grid.js
@@ -7,6 +7,7 @@ import "../widget/contextMenu.js"
 import "../widgetNg/button.js"
 
 import "../../3rdParty/jquery.blockUI.js"
+import "../../3rdParty/numbro.js"
 import "../../3rdParty/handsontable.js"
 import "../../component/grid/gridCreate.js"
 import "../../component/grid/gridEditor.js"
