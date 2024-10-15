56395264430c67cd0442d1aefa6cb75d431cd810
Roman Scherzer <r.scherzer@business-code.de>
2024-10-15 11:56:25 +0200

    Doku, added some comments

--- Server/src/main/java/de/businesscode/bcdui/toolbox/config/BareConfiguration.java
+++ Server/src/main/java/de/businesscode/bcdui/toolbox/config/BareConfiguration.java
@@ -181,7 +181,9 @@ public class BareConfiguration extends JNDIProvider {
             if (rollback) {
               log.error("Rolling back");
               con.rollback();
-            } else if (hasPendingChanges(con))
+            }
+            // only do a commit if there are pending changes (update/insert/delete/select from update statements)
+            else if (hasPendingChanges(con))
               con.commit();
           }
         } catch (SQLException ex) {
@@ -313,5 +315,6 @@ public class BareConfiguration extends JNDIProvider {
     return (Boolean)getConfigurationParameter(Configuration.DISABLE_CACHE, false);
   }
 
+  // always returns true, EE (when available) has a statement/connection wrapper which detects pendingChanges
   protected boolean hasPendingChanges(Connection con) throws SQLException { return true; }
 }
