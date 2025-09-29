2a883da9798d587002bd452e8c07cad6f2380911
Roman Scherzer <r.scherzer@business-code.de>
2025-09-29 13:56:00 +0200

    Build, don't use liquid as doT replacement

--- Client/src/js/import/3rdParty.js
+++ Client/src/js/import/3rdParty.js
@@ -1,20 +1,12 @@
 import "../3rdParty/modernizr.js"
 import "../3rdParty/log4javascript/log4javascript.js"
 import "../3rdParty/jquery.js"
+
+// if eval is allowed, import doT.js, otherwise don't use dott rendering/templates
 let _tpl = null;
 try {
   eval("const _evalTest=42;");
   _tpl = await import ("../3rdParty/doT.js");
-} catch (e) {
-  _tpl = await import ("../3rdParty/liquid.js").then(function(mod){  
-    window.bcdLiquid = new mod.Liquid();
-    window.doT = {};
-    doT.template = (tpl) => {
-      const liqTemplate = bcdLiquid.parse(tpl.replace(/=it\[/g, "=it.[").replace(/=it\./g, ""));
-      return (obj) => { return bcdLiquid.renderSync(liqTemplate, obj); }
-    }
-    doT.compile = (tpl) => { return doT.template(tpl); }
-  });
-}
+} catch (e) { }
 
 export const _bcdTemplateLib = _tpl;
