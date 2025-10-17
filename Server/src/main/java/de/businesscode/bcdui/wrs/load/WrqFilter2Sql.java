705795cb039a9efb3766986b74122f0282e66cd7
Roman Scherzer <r.scherzer@business-code.de>
2025-10-17 14:56:03 +0200

    use attribute getter instead direct access

--- Server/src/main/java/de/businesscode/bcdui/wrs/load/WrqFilter2Sql.java
+++ Server/src/main/java/de/businesscode/bcdui/wrs/load/WrqFilter2Sql.java
@@ -1,5 +1,5 @@
 /*
-  Copyright 2010-2021 BusinessCode GmbH, Germany
+  Copyright 2010-2025 BusinessCode GmbH, Germany
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -217,7 +217,7 @@ public class WrqFilter2Sql
     if( ignoreCase && ! bindingItem.isNumeric() ) {
       valueElement.setAttribute("value", valueElement.getAttribute("value").toLowerCase() );
       colExpr = "lower("+bindingItem.getQColumnExpression(false)+")";
-    } else if( useAggr && ! "none".equals(bindingItem.attributes.get("aggr")) ) {
+    } else if( useAggr && ! "none".equals(bindingItem.getAttribute("aggr")) ) {
       // If we do having for example, we are required to work on aggregated level for non-dims
       colExpr = bindingItem.getQColumnExpressionWithAggr(! wrqInfo.getGroupingBRefs().contains(bindingItem.getId()));
     } else {
