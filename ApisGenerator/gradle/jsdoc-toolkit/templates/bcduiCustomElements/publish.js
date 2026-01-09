3578cb9fcd3ae221e6f598ac8e19fb94ee8b2335
Roman Scherzer <r.scherzer@business-code.de>
2026-01-09 14:30:51 +0100

    fix core custom element building

--- ApisGenerator/gradle/jsdoc-toolkit/templates/bcduiCustomElements/publish.js
+++ ApisGenerator/gradle/jsdoc-toolkit/templates/bcduiCustomElements/publish.js
@@ -42,12 +42,12 @@ exports.publish = function(taffyData, opts, tutorials)
  */
 function customElementsCore( taffyData, path )
 {
-  var result = "";
-  var clazz = taffyData( { kind: "class", longname: "bcdui.core.Renderer", undocumented: { "!is": true } } ).get()[0];
-  result += printCustomTag( "bcd-"+clazz.name.toLowerCase(), clazz.memberof+"."+clazz.name+"Tag", clazz.params, "bcdui.factory.createRenderer" );
-  
-  fs.mkPath( path );
-  fs.writeFileSync( path+"/customElements.js", result, 'utf8');
+  var clazzes = taffyData( { kind: "class", longname: "bcdui.core.Renderer", undocumented: { "!is": true } } ).get();
+  if( clazzes.length === 1 ) {
+    var result = printCustomTag( "bcd-renderer", clazzes[0].memberof+".RendererTag", clazzes[0].params, "bcdui.factory.createRenderer");
+    fs.mkPath( path );
+    fs.writeFileSync( path+"/customElements.js", result, 'utf8');
+  }
 }
 
 /**
