cd0309bb596e9f1bb504d8e13d6775be9e839da7
martinbernemann <mbernemann@gmx.de>
2026-02-17 15:27:27 +0100

    Docu: JS docu markdown improve search

--- Docu/gradle/jsdoc-toolkit/templates/bcduiApiStubs/markdownClassGenerator.js
+++ Docu/gradle/jsdoc-toolkit/templates/bcduiApiStubs/markdownClassGenerator.js
@@ -1,5 +1,5 @@
 /*
-  Copyright 2010-2025 BusinessCode GmbH, Germany
+  Copyright 2010-2026 BusinessCode GmbH, Germany
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -31,25 +31,33 @@ class MarkdownClassGenerator extends MarkdownBaseGenerator {
    */
   startClass(classDoc) {
     // Class name and description
-    this.current += "# Class " + classDoc.longname + os.EOL;
-    this.current += (classDoc.classdesc || "") + os.EOL;
+    this.current += "# Class " + classDoc.name + os.EOL;
+    // To have the class name in header be the first line, which is important for search index,
+    // we write the package here below in a hidden span, and move it upo and show it with a kook on display
+    this.current += `<span hidden class='htmlPackage'>${classDoc.longname.substring(0, classDoc.longname.lastIndexOf("."))}</span>`;
+    this.current += os.EOL + this.jsLinksToMarkdownLinks((classDoc.classdesc || "")) + os.EOL;
 
     // Inheritance
-    if (classDoc.augments) this.current += "- Extends " + classDoc.augments[0];
-    let inheritsDP = this.inheritsFrom(this.taffyDb, classDoc.longname, "bcdui.core.DataProvider");
-    let inheritsRenderer = this.inheritsFrom(this.taffyDb, classDoc.longname, "bcdui.core.Renderer");
-    if( inheritsRenderer ) this.current += ", is a Renderer";
-    if( inheritsDP ) this.current += ", can act as a bcdui.core.DataProvider";
+    if (classDoc.augments) {
+      this.current += os.EOL + "_Extends_ [" + classDoc.augments[0] + "](" + classDoc.augments[0] + ".md)";
+      if( !["bcdui.core.DataProvider", "bcdui.core.Renderer"].includes(classDoc.augments[0]) ) {
+        let inheritsDP = this.inheritsFrom(this.taffyDb, classDoc.longname, "bcdui.core.DataProvider");
+        let inheritsRenderer = this.inheritsFrom(this.taffyDb, classDoc.longname, "bcdui.core.Renderer");
+        if (inheritsRenderer) this.current += ", is a Renderer";
+        if (inheritsDP) this.current += ", can act as a DataProvider";
+      }
+    }
 
     // Constructor
     this.current += os.EOL + "## Constructor";
     this.current += this.printCommentExamplesMandatories( classDoc, classDoc, false );
-    this.printMethod(classDoc, false, "");
+    this.printMethod(classDoc, false, "", true);
 
     // Constructor examples
+    let that = this;
     if(classDoc.examples) {
       this.current += os.EOL+"#### Examples"
-      classDoc.examples.forEach( e => this.current += os.EOL+"````js"+os.EOL+e+os.EOL+"````" );
+      classDoc.examples.forEach( e => this.current += os.EOL+"````js"+os.EOL+that.jsLinksToMarkdownLinks(e)+os.EOL+"````" );
     }
 
     // We now expect printMethod calls
@@ -61,26 +69,46 @@ class MarkdownClassGenerator extends MarkdownBaseGenerator {
    * @param methodDoc doclet
    * @param isInherited
    * @param methodName
+   * @param isConstructor
    */
-  printMethod(methodDoc, isInherited, methodName){
-    this.current += os.EOL + os.EOL + "---";
+  printMethod(methodDoc, isInherited, methodName, isConstructor = false){
+    this.current += os.EOL + os.EOL + ( methodName ? "---" : "" );
 
     // Method signature
     let docu = this.inheritDocu(this.taffyDb, methodDoc);
-    let reType = (docu.returns?.[0]?.type?.names?.[0] ?? "void").replaceAll(/([<|])/g, "\\$1");
+    this.current += os.EOL + (methodName ?? "### " + docu.name);
+
+    // We want to show the signature but not include it on the header because it would become part of the anchor
+    // which makes it hard to navigate to the element from search results or link externally
+    // So we add the signature here hidden and later restore it in a docsify hook
+    let retType = (docu.returns?.[0]?.type?.names?.[0] ?? "void").replaceAll(/([<|])/g, "\\$1");
     let argsList = (docu.params||[])
       .filter(p=>!p.name.includes("."))
       .map( p => p.name + (p.optional ? "?" : "") )
       .join(", ");
-    this.current += os.EOL + (methodName ?? "### ." + (docu.name || "") + "(" + argsList + ")" + ": " + reType);
+    let sig = "(" + argsList + ") &#x21FE; {" + retType + "}";
+    this.current += os.EOL + `<span style='display: none; font-size: 0.75em' class='htmlSignature' data-id='+docu.name'>${sig}</span>` + os.EOL;
 
     // Description
-    if( docu.description )this.current += os.EOL + docu.description;
-    if( docu.overrides ) this.current += os.EOL + "- Overrides " + docu.overrides;
-    else if( docu.inherits ) this.current += os.EOL + "- Inherited from " + docu.inherits.split("#")[0];
+    if( docu.description )this.current += os.EOL + this.jsLinksToMarkdownLinks(docu.description);
+    if( docu.overrides ) this.current += " \\" + os.EOL + "_Overrides_ " + docu.overrides;
+    else if( docu.inherits ) this.current += " \\" + os.EOL + "_Inherited from_ " + docu.inherits.split("#")[0];
 
     // Parameter list
-    this.current += this.printParams( docu.params );
+    let paramTable = this.printParams( docu.params );
+    this.current += paramTable;
+
+    // Description of return value
+    if( !isConstructor ) {
+      if( !paramTable ) this.current += "\\";
+      this.current += os.EOL + "**Returns** ";
+      if( retType !== "void" ) {
+        this.current += "{" + retType + "}:";
+        this.current += " " + this.jsLinksToMarkdownLinks((docu.returns[0].description ?? ""));
+      } else {
+        this.current += "{void}";
+      }
+    }
   }
 
   /**
@@ -89,8 +117,8 @@ class MarkdownClassGenerator extends MarkdownBaseGenerator {
    */
   finishClass(classDoc){
     let module = classDoc.longname.split(".").slice(-1).join("/");
-    let path =   this.opts.destination + "/../md/elements/"; // keep it flat, no module;
-    let fileName = classDoc.name + ".md";
+    let path = this.mdOutputElementsPath; // keep it flat, no module;
+    let fileName = classDoc.longname + ".md";
     fs.mkdirSync(path, { recursive: true });
     fs.writeFileSync( path + fileName, this.current );
     this.current = "";
--- Docu/gradle/jsdoc-toolkit/templates/bcduiApiStubs/markdownPackageGenerator.js
+++ Docu/gradle/jsdoc-toolkit/templates/bcduiApiStubs/markdownPackageGenerator.js
@@ -1,5 +1,5 @@
 /*
-  Copyright 2010-2025 BusinessCode GmbH, Germany
+  Copyright 2010-2026 BusinessCode GmbH, Germany
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 const os = require('os')
 const fs = require('fs');
 
+const helper = require('jsdoc/util/templateHelper');
 const {MarkdownBaseGenerator} = require('./markdownBaseGenerator');
 
 /**
@@ -24,25 +25,101 @@ const {MarkdownBaseGenerator} = require('./markdownBaseGenerator');
  */
 class MarkdownPackageGenerator extends MarkdownBaseGenerator {
 
-  current = "";
-
+  /**
+   * Package description
+   * @param packageDoc
+   */
   startPackage(packageDoc) {
-  }
+    this.currentPackage = "";  // Package overview page
+    this.current = "";         // Page per function
 
-  finishPackage(packageDoc) {
+    // Package header
+    this.currentPackage += `# Package ${packageDoc.longname}${os.EOL}${os.EOL}`;
+    if(packageDoc.description) this.currentPackage += this.jsLinksToMarkdownLinks(packageDoc.description) + os.EOL;
+
+    /**
+     * Because publish handles packages and classes separately, we need to take care for the classes here ourselves.
+     */
+    var allClasses = helper.find( this.taffyDb,
+      { kind: "class", memberof: packageDoc.longname,
+        access: { "!is": "private" }, abstract: {"!is": true}, virtual: {"!is": true} }
+    );
+    allClasses = allClasses.filter( _ => _.longname.indexOf("~") === -1 );
+    allClasses = allClasses.filter( _ => _.undocumented !== true );
+    allClasses.sort( (a,b) => a.name.localeCompare(b.name) );
+    let that = this;
+    // h4 to avoid this becoming part of sidebar
+    that.currentPackage += `${os.EOL}${os.EOL}----${os.EOL}<h4>Classes</h4>${os.EOL}${os.EOL}`;
+    allClasses.forEach( classDoc => {
+      that.currentPackage += `${os.EOL}${os.EOL}[${classDoc.name}](${classDoc.longname}.md)`;
+      if(classDoc.classdesc) that.currentPackage += "\\" + os.EOL + this.jsLinksToMarkdownLinks(classDoc.classdesc.split(/\.\s/)[0]) + ".";
+    });
+
+    /**
+     * Function section
+     * h4 to avoid this becoming part of sidebar
+     */
+    this.currentPackage += `${os.EOL}${os.EOL}----${os.EOL}<h4>Functions</h4>${os.EOL}${os.EOL}`;
   }
 
   /**
-   * Static package function
+   * List sub packages and
+   * Write this package's overview page
+   * @param packageDoc
+   */
+  finishPackage(packageDoc) {
+
+    // Properties
+    this.currentPackage += `${os.EOL}${os.EOL}----${os.EOL}<h4>Members</h4>${os.EOL}${os.EOL}`;
+    var allProperties = helper.find( this.taffyDb,
+      { kind: "constant", memberof: packageDoc.longname,
+        access: { "!is": "private" }, abstract: {"!is": true}, virtual: {"!is": true} }
+    );
+    let that = this;
+    allProperties.forEach(propDoc => {
+      // No link, they just appear in the package overview page with type and full description
+      that.currentPackage += `${os.EOL}${os.EOL}**${propDoc.name}** {${propDoc.type?.names?.[0] ?? "*"}}`;
+      if(propDoc.description) that.currentPackage += "\\" + os.EOL + that.jsLinksToMarkdownLinks(propDoc.description);
+    });
+
+    // List sub-packages
+    var subPackages = helper
+      .find( this.taffyDb, { kind: "namespace", memberof: packageDoc.longname, access: { "!is": "private" }  } )
+      .sort( (a,b) => a.name.localeCompare(b.name) );
+    this.currentPackage += `${os.EOL}${os.EOL}----${os.EOL}<h4>Subpackages</h4>${os.EOL}${os.EOL}`;
+    subPackages.forEach( subPackageDoc => {
+      that.currentPackage += `${os.EOL}${os.EOL}[${subPackageDoc.longname}](${subPackageDoc.longname}.md)`;
+      if(subPackageDoc.description) that.currentPackage += "\\" + os.EOL + this.jsLinksToMarkdownLinks(subPackageDoc.description.split(/\.\s/)[0]) + ".";
+    })
+
+    //----------------------------------
+    // Write package overview md page
+    let path =  this.mdOutputElementsPath; // keep it flat, no module;
+    let fileName = packageDoc.longname + ".md";
+    fs.mkdirSync(path, { recursive: true });
+    fs.writeFileSync( path + fileName, this.currentPackage );
+    this.currentPackage = "";
+  }
+
+  /**
+   * List classes on the package overview page
+   * And one page per static package function
    * @param docu
    */
   printPackageFunction(docu) {
 
+    // List function on the package overview page
+    this.currentPackage += `${os.EOL}${os.EOL}[${docu.name}()](${docu.longname}.md)`;
+    if(docu.description) this.currentPackage += "\\" + os.EOL + this.jsLinksToMarkdownLinks(docu.description.split(/\.\s/)[0]) + ".";
+
     // Title
-    this.current += "# " + docu.name + "()";
+    // Needs to be the first on markdown page so that is recognized as title and for example used in search results
+    // We add package information below so that we can create hook to show it on top in HTML
+    this.current += "# " + docu.name + "()" + os.EOL;
+    this.current += `<span hidden class='htmlPackage'>${docu.longname.substring(0, docu.longname.lastIndexOf('.'))}</span>` + os.EOL + os.EOL;
 
     // Description
-    if( docu.description ) this.current += os.EOL + docu.description;
+    if( docu.description ) this.current += os.EOL + this.jsLinksToMarkdownLinks(docu.description);
     if( docu.overrides ) this.current += os.EOL + "- Overrides " + docu.overrides;
     else if( docu.inherits ) this.current += os.EOL + "- Inherited from " + docu.inherits.split("#")[0];
 
@@ -50,17 +127,33 @@ class MarkdownPackageGenerator extends MarkdownBaseGenerator {
     this.current += os.EOL + this.printCommentExamplesMandatories(docu, docu, false, true)
 
     // Parameter description
-    this.current += this.printParams( docu.params );
+    this.current += "**Parameters**:";
+    let params = this.printParams( docu.params );
+    if( params ) this.current += os.EOL + params;
+    else this.current += " _None_";
+
+    // Return value
+    this.current += os.EOL+os.EOL+"**Returns**:";
+    if (docu.returns) {
+      var ret = docu.returns[0];
+      this.current += " {" + this.printCommentDataTypes(ret.type) + "}";
+      if (ret.description)
+        this.current += " - " + ret.description;
+      this.current += os.EOL;
+    } else {
+      this.current += " {void}" + os.EOL;
+    }
 
     // Examples
     if(docu.examples) {
-      this.current += os.EOL+"#### Examples"
+      let pl = docu.examples.length > 1 ? "s" : "";
+      this.current += `${os.EOL}**Example${pl}:**`;
       docu.examples.forEach( e => this.current += os.EOL+"````js"+os.EOL+e+os.EOL+"````" );
     }
 
     // Write file for this function
-    let path =   this.opts.destination + "/../md/elements/"; // keep it flat, no module;
-    let fileName = docu.name + ".md";
+    let path =  this.mdOutputElementsPath; // keep it flat, no module;
+    let fileName = docu.longname + ".md";
     fs.mkdirSync(path, { recursive: true });
     fs.writeFileSync( path + fileName, this.current );
     this.current = "";
